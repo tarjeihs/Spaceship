a7b88adf1628a1f946c56c8b6bc578b7d627313c
Tarjei H. S. <tarjeihs@hotmail.com>
2024-09-09 00:52:30 +0200

    API changes and misc. cleanup

--- Game/Source/Core/CameraSubsystem.cpp
+++ Game/Source/Core/CameraSubsystem.cpp
@@ -1,20 +1,53 @@
 #include "CameraSubsystem.h"
 
 #include "Core/Camera.h"
-#include "Core/Window.h"
 #include "Core/Input.h"
 #include "Core/KeyCode.h"
+#include "Core/Window.h"
+#include "Scene/Entity.h"
+#include "Scene/Registry.h"
 #include "Scene/Scene.h"
+#include "Scene/Component.h"
+#include "Renderer/Common/Material.h"
+#include "Renderer/Common/Mesh.h"
+
+IMesh* Mesh = nullptr;
+IMaterial* Material = nullptr;
+
+std::vector<SEntity> Entities;
 
 void PCameraSubsystem::OnAttach()
 {
     PCamera* Camera = GetScene()->GetCamera();
-    Camera->SetPerspectiveProjection(glm::radians(45.0f), GetWindow()->GetAspectRatio(), 0.1f, 1000.0f);
 
     Position = glm::vec3(-2.5f, 0.0f, 0.0f);
     Rotation = glm::vec3(0.0f, 0.0f, 0.0f);
 
     GetWindow()->OnWindowFocusDelegate.Bind(this, &PCameraSubsystem::OnWindowFocusCallback);
+
+    GetScene()->GetAssetManager()->LoadAsset("/home/tarjeihs/Workspace/Spaceship/Game/Content/Monkey.glb", "Mesh", "MonkeyMesh");
+    GetScene()->GetAssetManager()->LoadAsset("/home/tarjeihs/Workspace/Spaceship/Game/Content/Material.mat", "Material", "MonkeyMaterial");
+    
+    const SAsset* MeshAsset = GetScene()->GetAssetManager()->GetAsset("Mesh", "MonkeyMesh");
+    const SAsset* MaterialAsset = GetScene()->GetAssetManager()->GetAsset("Material", "MonkeyMaterial");
+    
+    Mesh = MeshAsset->GetObject<IMesh>();
+    Material = MaterialAsset->GetObject<IMaterial>();
+
+    Mesh->ApplyMaterial(Material);
+
+    for (int32_t Index = 0; Index < 10; ++Index)
+    {
+        STransform Transform;
+        Transform.Translation.x = (Index % 10) * 2.0f;
+        Transform.Translation.z = ((float)Index / 10) * 2.0f;
+
+        SEntity Entity = GetScene()->GetRegistry()->CreateEntity();
+        Entity.AddComponent<STransformComponent>(Transform);
+        Entity.AddComponent<SMeshComponent>(Mesh);
+
+        Entities.push_back(Entity);
+    }
 }
 
 void PCameraSubsystem::OnDetach()
@@ -27,34 +60,13 @@ void PCameraSubsystem::OnUpdate(float DeltaTime)
     if (!GetWindow()->IsFocused())
     {
         return;
-    }
+    }   
+
+    //Entity.GetComponent<STransformComponent>().Transform.Rotation.y -= 0.00025f;
 
     PCamera* Camera = GetScene()->GetCamera();
 
-    if (PInput::GetKeyPressed(RK_KEY_W))
-    {
-        Position += GetForwardVector() * KeyboardMovementSpeed * DeltaTime;
-    }
-    if (PInput::GetKeyPressed(RK_KEY_S))
-    {
-        Position -= GetForwardVector() * KeyboardMovementSpeed * DeltaTime;
-    }
-    if (PInput::GetKeyPressed(RK_KEY_D))
-    {
-        Position += GetRightVector() * KeyboardMovementSpeed * DeltaTime;
-    }
-    if (PInput::GetKeyPressed(RK_KEY_A))
-    {
-        Position -= GetRightVector() * KeyboardMovementSpeed * DeltaTime;
-    }
-    if (PInput::GetKeyPressed(RK_KEY_SPACE))
-    {
-        Position += GetUpVector() * KeyboardMovementSpeed * DeltaTime;
-    }
-    if (PInput::GetKeyPressed(RK_KEY_LEFT_CONTROL))
-    {
-        Position -= GetUpVector() * KeyboardMovementSpeed * DeltaTime;
-    }
+    constexpr float DeadzoneThreshold = 0.15f;
 
     if (int32_t JoystickID = PInput::GetFirstJoystickID(); JoystickID >= 0)
     {
@@ -67,19 +79,22 @@ void PCameraSubsystem::OnUpdate(float DeltaTime)
             Position -= GetUpVector() * JoystickMovementSpeed * DeltaTime;
         }
 
-        Position -= GetForwardVector() * JoystickMovementSpeed * PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_LEFT_Y) * DeltaTime;
-        Position += GetRightVector() * JoystickMovementSpeed * PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_LEFT_X) * DeltaTime;
+        float axisLeftY = PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_LEFT_Y);
+        float axisLeftX = PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_LEFT_X);
+        float axisRightY = PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_RIGHT_Y);
+        float axisRightX = PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_RIGHT_X);
 
-        Rotation.x -= PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_RIGHT_Y) * JoystickRotationSpeed * DeltaTime;  // Pitch in radians
-        Rotation.y += PInput::GetJoystickAxis(JoystickID, RK_GAMEPAD_AXIS_RIGHT_X) * JoystickRotationSpeed * DeltaTime;  // Yaw in radians
+        axisLeftY = (std::abs(axisLeftY) > DeadzoneThreshold) ? axisLeftY : 0.0f;
+        axisLeftX = (std::abs(axisLeftX) > DeadzoneThreshold) ? axisLeftX : 0.0f;
+        axisRightY = (std::abs(axisRightY) > DeadzoneThreshold) ? axisRightY : 0.0f;
+        axisRightX = (std::abs(axisRightX) > DeadzoneThreshold) ? axisRightX : 0.0f;
+
+        Position -= GetForwardVector() * JoystickMovementSpeed * axisLeftY * DeltaTime;
+        Position += GetRightVector() * JoystickMovementSpeed * axisLeftX * DeltaTime;
+
+        Rotation.x -= axisRightY * JoystickRotationSpeed * DeltaTime;  // Pitch in radians
+        Rotation.y += axisRightX * JoystickRotationSpeed * DeltaTime;  // Yaw in radians
     }
-
-    CurrentMousePosition = glm::vec2(PInput::GetMouseX(), PInput::GetMouseY());
-    const glm::vec2 DeltaMousePosition = CurrentMousePosition - LastMousePosition;
-    LastMousePosition = CurrentMousePosition;
-
-    Rotation.x -= DeltaMousePosition.y * MouseRotationSpeed * DeltaTime;  // Pitch in radians
-    Rotation.y += DeltaMousePosition.x * MouseRotationSpeed * DeltaTime;  // Yaw in radians
 
     Rotation.x = glm::clamp(Rotation.x, -glm::half_pi<float>() + 1e-4f, glm::half_pi<float>() - 1e-4f);
     Rotation.y = std::fmod(Rotation.y, glm::two_pi<float>());
@@ -92,8 +107,6 @@ void PCameraSubsystem::OnWindowFocusCallback(uint32_t Focused)
     if (Focused)
     {
         GetWindow()->WaitEventOrTimeout(0.1f);
-
-        LastMousePosition = glm::vec2(PInput::GetMouseX(), PInput::GetMouseY());
     }
 }
 
--- Game/Source/Core/CameraSubsystem.h
+++ Game/Source/Core/CameraSubsystem.h
@@ -5,7 +5,6 @@
 #include "Core/Subsystem.h"
 
 // Note: Convert radians to degrees to use an 0-360 rotation range
-
 class PCameraSubsystem : public ISubsystem 
 {
 public:
@@ -14,14 +13,8 @@ public:
         Position = glm::vec3(0.0f);
         Rotation = glm::vec3(0.0f);
 
-        LastMousePosition = glm::vec2(0.0f);
-        CurrentMousePosition = glm::vec2(0.0f);
-
         LastJoystickPosition = glm::vec2(0.0f);
         CurrentJoystickPosition = glm::vec2(0.0f);
-
-        KeyboardMovementSpeed = 5.0f;
-        MouseRotationSpeed = 2.0f;
 
         JoystickMovementSpeed = 4.0f;
         JoystickRotationSpeed = 2.0f;
@@ -43,14 +36,8 @@ protected:
     glm::vec3 Position;
     glm::vec3 Rotation;
 
-    glm::vec2 LastMousePosition;
-    glm::vec2 CurrentMousePosition;
-
     glm::vec2 LastJoystickPosition;
     glm::vec2 CurrentJoystickPosition;
-
-    float KeyboardMovementSpeed;
-    float MouseRotationSpeed;
 
     float JoystickMovementSpeed;
     float JoystickRotationSpeed;
